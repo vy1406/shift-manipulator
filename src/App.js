305405967a3c7bcd741a654a228a9eef654b8618
Sneguro4ka <yvova88@yahoo.com>
2019-08-13 18:44:54 +0300

    fixing App

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
-import Shifts from './components/shifts/Shifts';
-import Login from './components/login/Login'
-import About from './components/about/About'
-import Calendar from './components/calendar/Calendar'
+
 import Menu from './components/menu/Menu'
 
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Link } from 'react-router-dom'
 import { observer, inject } from 'mobx-react'
 
 import './App.css';
+import InnerMenu from './components/inner-menu/InnerMenu';
 
 @inject("generalStore")
 @observer
 class App extends Component {
 
+  renderLinks = () => {
+    return (
+      <div id="main-links">
+        <Link to="/login">Logins </Link>
+        <Link to="/about">About</Link>
+        <Link to="/Shifts">Shifts</Link>
+        <Link to="/calendar">calendar</Link>
+        <Link to="/about"><button className="btn">about</button></Link>
+        {/* <PrivateRoute exact path="/" component={HomePage} /> */}
+      </div>
+    )
+  }
   render() {
     return (
       <Router>
@@ -23,20 +33,10 @@ class App extends Component {
               <Menu />
             </div>
             <div className="header">
-              <div id="main-links">
-                <Link to="/login">Logins </Link>
-                <Link to="/about">About</Link>
-                <Link to="/Shifts">Shifts</Link>
-                <Link to="/calendar">calendar</Link>
-                <Link to="/about"><button className="btn">about</button></Link>
-                {/* <PrivateRoute exact path="/" component={HomePage} /> */}
-              </div>
+              {this.renderLinks()}
             </div>
             <div className="content">
-              <Route path="/login" exact component={Login} />
-              <Route path="/shifts" exact component={Shifts} />
-              <Route path="/about" exact component={About} />
-              <Route path="/calendar" exact component={Calendar} />
+              <InnerMenu />
             </div>
 
             <div className="footer"></div>
--- src/components/menu/Menu.js
+++ src/components/menu/Menu.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import './menu.css';
 
 class Menu extends Component {
@@ -12,7 +13,7 @@ class Menu extends Component {
                     </div>
                 </div>
                 <div className="controls">
-                    <button className="shift_request btn">Send shift request</button>
+                    {/* <button className="shift_request btn">Send shift request</button> */}
                     <button className="create_week btn">Create week</button>
                     <button className="give_options btn">Give options</button>
                     <button className="add_remove btn">Add/Remove Worker</button>
