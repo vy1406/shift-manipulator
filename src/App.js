492a0d6faefeb46355aeb4b335be632134e92c25
Sneguro4ka <yvova88@yahoo.com>
2019-08-22 13:37:57 +0300

    adding material-ui

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
 
   render() {
     return (
-      <div className="col container">
+      <div className="col">
         <div className="row">
           <div className="header">
             <LinksBar />
--- src/components/give-options/GiveOptions.js
+++ src/components/give-options/GiveOptions.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { observer, inject } from 'mobx-react'
 import Shift from '../shift/Shift';
 import axios from 'axios';
+import InputRange from 'react-input-range';
+import '../../../node_modules/react-input-range/lib/css/index.css'
+import Slider from '@material-ui/core/Slider';
 
 @inject("shiftsStore")
 @observer
@@ -12,12 +15,44 @@ class GiveOptions extends Component {
         this.props.shiftsStore.arrOptions = result.data
     }
 
+    renderControls = () => {
+
+        const style = {
+            color: "pink"
+        };
+
+        return (
+            <div className="row">
+                <div className="col s6 m6 l6">
+                    <Slider
+                        defaultValue={0}
+                        // getAriaValueText={this.valuetext}
+                        aria-labelledby="discrete-slider"
+                        valueLabelDisplay="auto"
+                        step={1}
+                        style={style}
+                        min={0}
+                        max={10}
+                    />
+                </div>
+                <div classNamee="col s6 m6 l6">
+                    
+                </div>
+            </div>
+        )
+    }
+
     render() {
         let arrOptions = this.props.shiftsStore.arrOptions
 
         return (
-            <div className="row">
-                {arrOptions.map((option, i) => <Shift key={i} day={i} option={option} />)}
+            <div className="container">
+                <div className="row">
+                    {arrOptions.map((option, i) => <Shift key={i} day={i} option={option} />)}
+                </div>
+                <div className="row">
+                    {this.renderControls()}
+                </div>
             </div>
         )
     }
