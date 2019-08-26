942f1368f206b7495d563613db9a5913eaa02d83
Sneguro4ka <yvova88@yahoo.com>
2019-08-26 17:32:18 +0300

    choose login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -9,9 +9,32 @@ import AddUser from '../add-user/AddUser';
 @observer
 class Login extends Component {
 
-    constructor() {
-        super()
-        this.state = {
+    // for other selectors
+    handleChange = (event) => {
+        this.setState({ value: event.target.value });
+    };
+
+    // for other selectors
+    componentDidMount() {
+        let selects = document.querySelectorAll('select');
+        M.FormSelect.init(selects, {});
+    }
+
+    async componentWillMount() {
+        let users = await axios.get("http://localhost:8080/users")
+        this.props.generalStore.createDropDownUserSelect(users.data)
+    }
+
+    handleInput = event => { this.props.generalStore.handleInput(event.target.className, event) }
+
+    login = () => { console.log(this.props.generalStore.curUser) }
+
+    onChange = event => { console.log(event) }
+
+    renderOtherDropDowns = () => {
+        console.log("you can see other drop downs here...")
+        const url = "https://stackoverflow.com/questions/28868071/onchange-event-using-react-js-for-drop-down"
+        const obj = {
             options: [
                 {
                     name: 'Selec2t…',
@@ -32,66 +55,48 @@ class Login extends Component {
             ],
             value: '?'
         }
-    }
 
-    handleChange = (event) => {
-        this.setState({ value: event.target.value });
-    };
-
-    componentDidMount() {
-        let selects = document.querySelectorAll('select');
-        M.FormSelect.init(selects, {});
-    }
-
-    async componentWillMount() {
-        let users = await axios.get("http://localhost:8080/users")
-        this.props.generalStore.createDropDownUserSelect(users.data)
-    }
-
-    handleInput = event => { this.props.generalStore.handleInput(event.target.className, event) }
-
-    login = () => { this.props.generalStore.login() }
-
-    onChange = event => { console.log(event) }
-
-    render() {
-        const { options, value } = this.state;
-
-        const options3 = [
-            { label: "a", value: "a", disabled: true },
-            { label: "b", value: "b" },
-            { label: "C", value: "C" }
-        ]
+        const { options, value } = obj;
 
         return (
-            <div className="container">i'm a login.
-            <hr></hr>
+            <div className="row">
+                <div className="input-field col s4">
+                    <select className="browser-default" onChange={this.onChange}>
+                        <option value="" disabled selected>Choose your option</option>
+                        <option value="1">Option 1</option>
+                        <option value="2">Option 2</option>
+                        <option value="3">Option 3</option>
+                    </select>
+                </div>
+                <div className="input-field col s4">
+                    <select onChange={this.handleChange} value={value}>
+                        {options.map(item => (
+                            <option
+                                key={item.value} value={item.value}>
+                                {item.name}
+                            </option>
+                        ))}
+                    </select>
+                    <h1>Favorite letter: {value}</h1>
+                </div>
+            </div>
+        )
+    }
+
+    render() {
+        console.log("you have here other dropDowns")
+        return (
+            <div className="container">
                 <div className="row">
-                    <div className="input-field col s4">
-                        <select className="browser-default" onChange={this.onChange}>
-                            <option value="" disabled selected>Choose your option</option>
-                            <option value="1">Option 1</option>
-                            <option value="2">Option 2</option>
-                            <option value="3">Option 3</option>
-                        </select>
-                    </div>
-                    <div className="input-field col s4">
-                        <select onChange={this.handleChange} value={value}>
-                            {options.map(item => (
-                                <option
-                                    key={item.value} value={item.value}>
-                                    {item.name}
-                                </option>
-                            ))}
-                        </select>
-                        <h1>Favorite letter: {value}</h1>
-                    </div>
-                    <div className="col s4">
+                    <div className="col s12 m6 l6">
                         <Select id={'dropdown'}
                             options={this.props.generalStore.selectOptions}
                             defaultValue={{ label: "Select user", value: 0 }}
-                            onChange={value => console.log(value)}>
+                            onChange={value => this.props.generalStore.curUser = value.value}>
                         </Select>
+                    </div>
+                    <div className="col s12 m6 l6">
+                            <a className="waves-effect waves-light btn" onClick={this.login}><i className="material-icons right">cloud</i>Login</a>
                     </div>
                 </div>
                 <AddUser />
